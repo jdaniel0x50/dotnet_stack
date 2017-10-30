78d23770486add377f7b4fc6179c553e76182f13
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-30 12:30:51 -0500

    initializing dotnet core repository

---
+++ 0_fundamentals/00_intro_first_csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _00_intro_first_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
